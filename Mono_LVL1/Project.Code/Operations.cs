56983b467f4530c8b1ecf4f31f25e4c63dc79724
Your Name <you@example.com>
2016-09-30 15:08:33 +0200

    Initial commit

---
+++ Mono_LVL1/Project.Code/Operations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Code
+{
+    public static class Operations
+    {
+        static String enlist = "ENLIST";
+        static String display = "DISPLAY";
+    }
+}
